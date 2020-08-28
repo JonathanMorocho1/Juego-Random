9864a698015ff18c38f219411aca40ffcddeefa4
JonathanMorocho1 <70121868+JonathanMorocho1@users.noreply.github.com>
2020-08-27 22:24:09 -0500

    Update Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prueba;
 
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- *
- * @author ANDRES
- */
 public class Prueba {
 
     int num = 0, aux = 0;
