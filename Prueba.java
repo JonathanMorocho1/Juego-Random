ab78f7c28902a78a406a0c0fa19d5681a1905457
JonathanMorocho1 <70121868+JonathanMorocho1@users.noreply.github.com>
2020-08-27 22:07:12 -0500

    Add files via upload

---
+++ Prueba.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ANDRES
+ */
+public class Prueba {
+
+    int num = 0, aux = 0;
+    boolean incog;
+    Scanner sc = new Scanner(System.in);
+    int[] numeros = new int[5];
+
+    public void arrays() {
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("ingrese el numero " + i + " del arreglo: ");
+            num = sc.nextInt();
+            incog = true;
+            for (int j = 0; j < numeros.length; j++) {
+                if (num == numeros[j]) {
+                    i--;
+                    incog = false;
+                    System.out.println("Error ingresaste un número repetido");
+                }
+            }
+            if (incog == true) {
+                numeros[i] = num;
+            }
+        }
+
+    }
+    int ram = 0;
+    public void random() {
+        if (incog == true) {
+            Random r = new Random();
+            ram = r.nextInt(10) + 1;
+            System.out.println("El Random es: " + ram);
+            for (int i = 0; i < numeros.length; i++) {
+                if (ram == numeros[i]) {
+                    System.out.println("Ganaste");
+                    aux = numeros[i];
+                }
+            }
+        }
+        if(ram != aux){
+            System.out.println("Perdiste");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Prueba ajem = new Prueba();
+        ajem.arrays();
+        ajem.random();
+    }
+}
